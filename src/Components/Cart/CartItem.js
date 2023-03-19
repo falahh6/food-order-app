419e8861331d807e5b86a05b05c1cdb7a8697e76
Mohammed Falah <falahsss900@gmail.com>
2023-03-19 14:12:14 +0530

    Commit witth '2 issues'

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -3,11 +3,6 @@ import styles from './CartItem.module.css'
 const CartItem = (props) =>{
 
     const price = `$${props.price.toFixed(2)}`
-    const itemQ = `${props.amount}`
-
-    const onAddItemHandler = (id) => {}
-
-    const onRemoveItemHandler = (item) => {}
 
     return (
         <li className={styles['cart-item']}>
@@ -15,12 +10,12 @@ const CartItem = (props) =>{
             <h2>{props.name}</h2>
             <div className={styles.summary}>
                 <span className={styles.price}>{price}</span>
-                <span className={styles.amount}>x{itemQ}</span>
+                <span className={styles.amount}>x{props.amount}</span>
             </div>
             </div>
             <div className={styles.actions}>
-                <button onClick={onRemoveItemHandler}> - </button>
-                <button onClick={onAddItemHandler}> + </button>
+                <button onClick={props.onRemoveItem}> - </button>
+                <button onClick={props.onAdd}> + </button>
             </div>
         </li>
     )
